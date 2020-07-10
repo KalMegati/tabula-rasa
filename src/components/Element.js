bf61a78050ae451b5faa680aab4f560d2024623a
kalmegati <kalmegati@gmail.com>
2020-07-10 17:06:26 -0400

    can customize appearance of Element

--- src/components/Element.js
+++ src/components/Element.js
@@ -3,38 +3,59 @@ import { connect } from 'react-redux';
 
 class Element extends React.Component {
 
+  renderOrNot = (className, entry, index, legend) => {
+    // debugger
+    if (legend[index] === "0") {
+      return null
+    } else {
+      return (<div className={className}>
+        {entry}
+      </div>)
+    }
+  }
+
+  renderOrBox = (entry1, entry2, index1, index2, legend) => {
+    if (legend[index1] === "0" && legend[index2] === "0") {
+      return null
+    } else {
+      return (<div className="card-header">
+        {this.renderOrNot("single-entry", entry1, index1, legend)}
+        {this.renderOrNot("short-entry", entry2, index2, legend)}
+      </div>)
+    }
+  }
+
+  renderOrLine = (index, legend) => {
+    if (legend[index] === "0") {
+      return null
+    } else {
+      return <hr />
+    }
+  }
+
   render() {
 
-    const  {
+    const {
       icon1, icon2, icon3,
       label1, label2, label3,
-      text1, text2, text3
+      text1, text2, text3,
+      active
     } = this.props.element
 
-    return (
+    const activity_array = active.split("")
 
+    return (
       <div className="element-screen">
+        <p>{active}</p>
         <div className="card text-white bg-primary mb-3">
-          <div className="card-header">
-            <div className="single-entry">{icon1}</div>
-            <div className="short-entry">{label1}</div>
-          </div>
-          <div className="card-header">
-            <div className="single-entry">{icon2}</div>
-            <div className="short-entry">{label2}</div>
-          </div>
-          <div className="card-header">
-            <div className="single-entry">{icon3}</div>
-            <div className="short-entry">{label3}</div>
-          </div>
-          <div className="long-entry card-body">{text1}</div>
-          <div className="long-entry card-body">{text2}</div>
-          <div className="long-entry card-body">{text3}</div>
-          {/* <div class="card-header">Header</div>
-          <div class="card-body">
-            <h4 class="card-title">Primary card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div> */}
+          {this.renderOrBox(icon1, label1, 0, 1, activity_array)}
+          {this.renderOrBox(icon2, label2, 2, 3, activity_array)}
+          {this.renderOrBox(icon3, label3, 4, 5, activity_array)}
+          {this.renderOrNot("long-entry card-body", text1, 6, activity_array)}
+          {this.renderOrLine(7, activity_array)}
+          {this.renderOrNot("long-entry card-body", text2, 7, activity_array)}
+          {this.renderOrLine(8, activity_array)}
+          {this.renderOrNot("long-entry card-body", text3, 8, activity_array)}
         </div>
       </div>
 
--- src/screens/ElementScreen.js
+++ src/screens/ElementScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createElement } from '../actions/actions'
 import { withRouter } from 'react-router-dom';
+import Element from '../components/Element'
 
 class ElementScreen extends React.Component {
 
@@ -16,39 +17,82 @@ class ElementScreen extends React.Component {
     text1: "",
     text2: "",
     text3: "",
-    active: "123456789"
+    active_array: [1, 2, 3, 4, 5, 6, 7, 8, 9]
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
-    this.props.createElement(this.state, this.props.history)
+    this.props.createElement(
+      {...this.state, active: this.state.active_array.join("")},
+      this.props.history
+    )
     // .then(response => this.props.history.push(`/characters/${this.state.character_id}`))
     // setTimeout(() => this.props.history.push(`/characters/${this.state.character_id}`), 1000)
     // this.props.history.push(`/characters/${this.state.character_id}`)
   }
 
+  makeCheckboxes = () => {
+    return [0,1,2,3,4,5,6,7,8].map(dex =>
+      <div class="custom-control custom-checkbox">
+        <input
+          type="checkbox" class="custom-control-input"
+          id={`customCheck${dex+1}`} name={dex}
+          onChange={(e) => this.handleCheck(e)}
+        />
+        <label class="custom-control-label"
+          for={`customCheck${dex+1}`}
+        >Field {dex+1}</label>
+      </div>
+    )
+  }
+
+  handleCheck = (e) => {
+    // e.target.checked === false
+    const box = document.getElementById(`input${e.target.name}`)
+    box.disabled = !box.disabled
+    const toggle = () => {
+      if (this.state.active_array[e.target.name] === 0) {
+        return (parseInt(e.target.name)+1)
+      } else {
+        return 0
+      }
+    }
+    const lead = this.state.active_array.slice(0, e.target.name)
+    const lag = this.state.active_array.slice(parseInt(e.target.name)+1)
+    this.setState({active_array: [ ...lead, toggle(), ...lag ]})
+  }
+
   render() {
     return (
       <div>
-          <div className="spacer-5"></div>
-          <div className="spacer-5"></div>
-          <div className="element-screen">
-            <form onChange={e => this.setState({[e.target.name]: e.target.value})} onSubmit={e => this.handleSubmit(e)}>
-              <input type="text" className="single-entry" name="icon1" value={this.state.icon1} />
-              <input type="text" className="short-entry" name="label1" value={this.state.label1} />
-              <input type="text" className="single-entry" name="icon2" value={this.state.icon2} />
-              <input type="text" className="short-entry" name="label2" value={this.state.label2} />
-              <input type="text" className="single-entry" name="icon3" value={this.state.icon3} />
-              <input type="text" className="short-entry" name="label3" value={this.state.label3} />
-              <textarea className="long-entry" name="text1" value={this.state.text1} />
-              <textarea className="long-entry" name="text2" value={this.state.text2} />
-              <textarea className="long-entry" name="text3" value={this.state.text3} />
-              <input type="submit" />
-            </form>
-          </div>
-          <div className="spacer-5"></div>
-          <div className="spacer-5"></div>
-          <div className="element-screen">Kiryu</div>
+        {/* <div className="spacer-5"></div>
+        <div className="spacer-5"></div> */}
+        <div className="element-screen">
+          <h3>Element Text</h3>
+          <form onChange={e => this.setState({[e.target.name]: e.target.value})} onSubmit={e => this.handleSubmit(e)}>
+            <input type="text" className="single-entry" id="input0" name="icon1" value={this.state.icon1} />
+            <input type="text" className="short-entry" id="input1" name="label1" value={this.state.label1} />
+            <input type="text" className="single-entry" id="input2" name="icon2" value={this.state.icon2} />
+            <input type="text" className="short-entry" id="input3" name="label2" value={this.state.label2} />
+            <input type="text" className="single-entry" id="input4" name="icon3" value={this.state.icon3} />
+            <input type="text" className="short-entry" id="input5" name="label3" value={this.state.label3} />
+            <textarea className="long-entry" id="input6" name="text1" value={this.state.text1} />
+            <textarea className="long-entry" id="input7" name="text2" value={this.state.text2} />
+            <textarea className="long-entry" id="input8" name="text3" value={this.state.text3} />
+            <input type="submit" />
+          </form>
+        </div>
+        {/* <div className="spacer-5"></div>
+        <div className="spacer-5"></div> */}
+        <Element element={{...this.state, active: this.state.active_array.join("")}}/>
+        {/* <div className="spacer-5"></div>
+        <div className="spacer-5"></div> */}
+        <div className="element-screen">
+
+          <h3>Disabled Fields</h3>
+          {this.makeCheckboxes()}
+
+        </div>
       </div>
     );
   }
